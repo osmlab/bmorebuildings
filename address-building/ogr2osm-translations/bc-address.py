2733f2fd884e5a4e7160d653dec79de4cc609ea2
Elliott Plack <elliott.plack@gmail.com>
2015-01-12 23:19:53 -0500

    added some experimental code for replacing
    code should replace any of the special name types in the data. also
    added some comments.

--- address-building/ogr2osm-translations/bc-address.py
+++ address-building/ogr2osm-translations/bc-address.py
@@ -54,16 +54,38 @@ def translateType(stType):
     return stTypeExpanded
 
 def caseStreetName(stName):
+    import re
 
+    # capitalize each word that is separated by a space
     address = stName
     stNameCased = ' '.join([word.capitalize() for word in address.split()])
+
+    # special names that need to be replaced
+    rep = {"O'donnell": "O'Donnell", "Mcallister": "McAllister",
+        "Mccabe": "McCabe", "Mcclean": "McClean",
+        "Mccollough": "McCollough", "Mccomas": "McComas",
+        "Mcculloh": "McCulloh", "Mccurley": "McCurley",
+        "Mcdonogh": "McDonogh", "Mcelderry": "McElderry",
+        "Mchenry": "McHenry", "Mckay": "McKay",
+        "Mckean": "McKean", "Mckendree": "McKendree",
+        "Mckewin": "McKewin", "Mcmechen": "McMechen",
+        "Mcphail": "McPhail", "Mcteague": "McTeague",
+        "St.": "Saint", "St Paul": "Saint Paul",
+        "St Matthews": "Saint Matthews"}
+
+    # replace any of the rep names above
+    # code source from http://stackoverflow.com/a/6117124/2105596
+    rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+    pattern = re.compile("|".join(rep.keys()))
+    stNameCased = pattern.sub(lambda m: rep[re.escape(m.group(0))], stNameCased)
 
     return stNameCased
 
 def translateDirection(dirAbbr):
     suffixlookup = {}
 
-    # Directions
+    # Directionals that need to be expanded. The code replaces the
+    # item before the colon with the one after.
 
     suffixlookup.update({'E':'East'})
     suffixlookup.update({'S':'South'})
@@ -81,10 +103,11 @@ def filterTags(attrs):
 
     tags = {}
 
-    #automagically convert names
+    # clear vars
     x = ''
     y = ''
 
+    # convert names if there is a name
     if attrs['ST_NAME']:
         tags.update({'addr:street':' '.join([x for x in (
             translateDirection(attrs['ST_DIR']),
@@ -96,12 +119,15 @@ def filterTags(attrs):
         tags.update({'addr:state':'MD'})
         tags.update({'addr:country':'US'})
 
+    # if the address number isn't zero, compile the number
+    # TODO: need to look into this fractional bit
     if attrs['ADDR_NUMBE'] != '0':
        tags.update({'addr:housenumber':' '.join([y for y in (
             attrs['ADDR_NUMBE'],
             attrs['ADDR_FRAC']) if y])
             })
 
+    # convert zipcode
     if attrs['ZIP_CODE']:
         tags.update({'addr:postcode':attrs['ZIP_CODE']})
 
